d85217f16b8c381b50799e9e70261a82a62cbca5
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-12-03 17:19:37 +0000

    locating agents on random network nodes

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 import osmnx
-
-
-class EvacuationAgent(Agent):
-    """A person with an age"""
-    def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
-        self.age = 25
-
-    def step(self):
-        pass
+from networkx import MultiDiGraph
 
 
 class EvacuationModel(Model):
     """A model with some number of agents"""
-    def __init__(self, num_agents, osm_file):
+    def __init__(self, num_agents, osm_file, seed=None):
+        self._seed = seed
         self.num_agents = num_agents
         self.schedule = RandomActivation(self)
-        self.G = osmnx.graph_from_file(osm_file)
+        self.G: MultiDiGraph = osmnx.graph_from_file(osm_file)
         # Create agents
         for i in range(self.num_agents):
             a = EvacuationAgent(i, self)
@@ -27,3 +19,15 @@ class EvacuationModel(Model):
     def step(self):
         """Advance the model by one step."""
         self.schedule.step()
+
+
+class EvacuationAgent(Agent):
+    """A person with an age"""
+    def __init__(self, unique_id, model: EvacuationModel):
+        super().__init__(unique_id, model)
+        self.location = model.random.choice(list(model.G.nodes))
+
+    def step(self):
+        print(self.location)
+
+
